775ac1a6327dc2b6520fbe5c1bddadccbcd42eaa
sean-flores <fad0791@dlsud.edu.ph>
2023-03-20 22:41:05 +0800

    230320

---
+++ indexActivity.js
@@ -0,0 +1,21 @@
+console.log("Hello, Welcome to De La Salle University!");
+
+let firstName = ' Alvin Sean'
+let lastName = 'Flores'
+let age = 22
+let hobbies = ["Playing Video games", "PLaying Volleyball", " Listening to music", 'Working-out', "Watching movies" ]
+let homeAddress = {
+	housenumber: "Lt 4 Blk 5 Phs 1 Sec 3",
+	street: 'Paradiwa',
+	subdivision: 'Belverdere',
+	barangay: 'Paradahan 1',
+	city: 'Tanza',
+	province: 'Cavite',
+	zipcode: 4108
+};
+
+console.log(firstName)
+console.log(lastName)
+console.log(age)
+console.log(hobbies)
+console.log(homeAddress)
